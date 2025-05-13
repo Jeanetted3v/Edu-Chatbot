8fb0d3391e967e41f309d92c034bc8b8a20a86e2
Jeanette <jeanetted3v@gmail.com>
2025-05-13 14:36:48 +0800

    resolve @lib/utils path issue

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -1,10 +1,19 @@
+
+// telling vscode to treat this file as a NextConfig file
 /** @type {import('next').NextConfig} */
+// import Node.js built-in 'path' module. Create absolute paths in a platform-safe way
+const path = require('path');
+
+// Disable ESLint during build time (next build). Need to come back to fix this. 
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  // To resolve the @lib alias to the src/lib directory
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
-
+// Exports the configuration object so Next.js can use it.
 module.exports = nextConfig;
